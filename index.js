e24ff23c9fd9eba836427ef3c74f7eb5dd2cdf9b
EricWithC04 <negativegame117@gmail.com>
2024-04-22 18:47:54 -0300

    fear: charge the words with a input

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+const resultsContainer = document.getElementById('results');
+const formContainer = document.getElementById('formulario');
 const threshold = 0.9;
 
-const evalue = async () => {
+const evalue = async (sentences) => {
     console.log('Evaluando frases...');
+    resultsContainer.innerHTML = 'Evaluando frases...';
     await toxicity.load(threshold).then(model => {
-        const sentences = ['you suck', 'motherfucker'];
+        // const sentences = ['you suck', 'you motherfucker', 'fuck'];
         
         model.classify(sentences).then(predictions => {
             console.log(predictions);
+            resultsContainer.innerHTML = JSON.stringify(predictions, null, 2);
         });
     });
     console.log('Finalizado!');
 }
 
-evalue()
+formContainer.addEventListener('submit', (e) => {
+    e.preventDefault()
+    evalue([e.target[0].value])
+});
